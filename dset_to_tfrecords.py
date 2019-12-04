342ed94c38fbb18d757fac198dc585a0cc247668
katherinekowalski <kasia4@stanford.edu>
2019-12-04 00:44:06 -0800

    Update dset_to_tfrecords.py

--- dset_to_tfrecords.py
+++ dset_to_tfrecords.py
@@ -12,44 +12,42 @@ def _int64_feature(value):
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-DATA_DIR = "../data"
-OUT_DIR
+DATA_DIR = "D:\\EE269\\"
+OUT_DIR = "D:\\EE269\\pythonOutput"
 
 def write(dset="train"):
   out_filename = os.path.join(OUT_DIR, f"{dset}.tfrecords")
   if dset == "train":
     x_filename = 'train.mat'
     idf = "X_train_wd"
-    y_fn = "ytrain.mat"
+    y_fn = "train.mat"
     idf_y = "Y_train_wd"
   elif dset == "val":
     x_filename = 'val.mat'
     idf = "X_val_wd"
-    y_fn = "yval.mat"
+    y_fn = "val.mat"
     idf_y = "Y_val_wd"
   elif dset == "test":
     x_filename = 'test.mat'
     idf = "X_test_wd"
-    y_fn = "ytest.mat"
+    y_fn = "test.mat"
     idf_y = "Y_test_wd"
   else:
     raise ValueError
 
   xdata = h5py.File(os.path.join(DATA_DIR, x_filename), "r")
-  ydata = loadmat(os.path.join(DATA_DIR, y_fn))[idf_y][0]
+  ydata = h5py.File(os.path.join(DATA_DIR, y_fn))
 
   file_refs = xdata[idf]
   writer = tf.io.TFRecordWriter(out_filename)
   l = list(enumerate(file_refs))
   random.shuffle(l)
   for i, userref in tqdm(l):
-    labels = ydata[i]
+    labels = ydata[ydata["Y_train_wd"][i][0]]
     examples = xdata[userref[0]]
     user_examples = examples.shape[0]
     for j in range(user_examples):
-      print (labels[j], labels[j].dtype)
-      print (examples[j].shape)
-      feature = {'train/label': _int64_feature(labels[j]),
+      feature = {'train/label': _int64_feature(int(labels[0][j])),
                  'train/image': _bytes_feature(tf.compat.as_bytes(examples[j].tostring()))}
       # Create an example protocol buffer
       example = tf.train.Example(features=tf.train.Features(feature=feature))
